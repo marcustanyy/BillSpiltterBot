be02f9a570f587feadd3ba649c9e0aa1cb16a98a
climbalot <florencelry@gmail.com>
2022-08-24 15:16:01 +0800

    flask setup

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import ast
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Users(Resource):
+    # methods go here
+    def get(self):
+        print("Users")
+    
+class Locations(Resource):
+    # methods go here
+    def get(self):
+        print("Locations")
+    
+api.add_resource(Users, '/users')  # '/users' is our entry point for Users
+api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
